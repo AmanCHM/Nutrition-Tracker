df74e172f5ff5b26381602326ed657cd2ef76fb0
imamankmr <amanjnvr@gmail.com>
2024-11-30 01:57:20 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,34 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAgAyIKmfDh3cO6TNQ2sWBQhjn-UrHbVyY",
+  authDomain: "nutrition-tracker-f149b.firebaseapp.com",
+  projectId: "nutrition-tracker-f149b",
+  storageBucket: "nutrition-tracker-f149b.firebasestorage.app",
+  messagingSenderId: "263013717884",
+  appId: "1:263013717884:web:2d867e354e578212edf6f9"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
