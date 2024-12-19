dd3f2a050c790d49fe07d11eaebde891aba17c22
AmanCHM <aman.kumar@chicmicstudios.in>
2024-12-19 11:41:01 +0530

    app-build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 2500 
+      },
     resolve: {
         alias: {
             '@': '/src',
